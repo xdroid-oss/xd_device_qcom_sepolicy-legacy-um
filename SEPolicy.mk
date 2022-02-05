5b52203463ca9dc437c3a9aae4e8910c29ea0dde
xyzuan <xyzuannihboss@gmail.com>
2022-02-05 02:33:26 +0000

    sepolicy_legacy_um: Refractor for xdsp

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -48,4 +48,4 @@ ifneq (,$(filter sdm845 sdm710 sdm660 msm8937 msm8953 msm8996 msm8998, $(TARGET_
     endif
 endif
 
--include device/arrow/sepolicy/qcom/sepolicy.mk
+-include device/xdroid/sepolicy/qcom/sepolicy.mk
